802890362777b78c2fadfaa02c897fcdb94a1f7b
omartemirgali <temirgaliomar@gmail.com>
2022-10-19 14:41:34 +0600

    Adding main website elements, and upload image functionality

--- test.py
+++ test.py
@@ -1,6 +1,43 @@
-from flask import Flask
+import os, re
+from flask import Flask, flash, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
+app.secret_key = "somesecretkey"
+app.config['UPLOAD_PATH'] = 'static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main_page():
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):   
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        flash('Image successfully uploaded')
+        return render_template('home.html', filename=filename)
+    else: 
+        flash('Image extension should be png, jpg, jpeg or gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+if __name__ == '__main__':
+    app.run()
